66cb9b668dc7a727a41051fbe5ac3263e504a2d2
Ryzen <ryanrahmatullah8@gmail.com>
2026-05-21 22:03:54 +0700

    Update supabaseClient.ts

--- config/supabaseClient.ts
+++ config/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://oxycoeaftdpfezcmewmv.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im94eWNvZWFmdGRwZmV6Y21ld212Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwMzI2MzksImV4cCI6MjA5MjYwODYzOX0.qsqipo1udTzPahh8FjF1I0ZJBHh6THGV4d-oRw6XzgI';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     auth: {
